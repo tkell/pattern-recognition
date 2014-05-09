485ba9433593ce307b483f6bff36b2569ccb0b48
tkell <thor.kell@mail.mcgill.ca>
2014-05-09 14:46:16 -0400

    Adding start of classification feedback

--- scales.js
+++ scales.js
@@ -4,7 +4,7 @@ var pentatonicScale = [2, 2, 3, 2, 3];
 var diatonicMajorScale = [2, 2, 1, 2, 2, 2, 1];
 var diatonicMinorScale = [2, 1, 2, 2, 1, 2, 2];
 var diatonicBothScale = [2, 1, 1, 1, 2, 2, 1, 1]; // has flat 3 and flat 7
-var trumpetScale = [1, 1, 1]; // has flat 3 and flat 7
+var trumpetScale = [1, 1, 1];
 
 var scales = {
     'chromatic': chromaticScale,
